6010d1a27571163e8afb1396006f2475f08873ba
Haldun Altuntop <altuntophaldun@gmail.com>
2026-03-12 18:47:44 +0300

    Favori kitapların fragmanına başlandı. Recycer view bağlandı.

--- app/src/main/java/arc/haldun/elib/FavouritesFragment.kt
+++ app/src/main/java/arc/haldun/elib/FavouritesFragment.kt
@@ -1,15 +1,16 @@
 package arc.haldun.elib
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import arc.haldun.elib.models.FavBooksListModel
+import arc.haldun.elib.viewmodels.FavBooksListViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -17,15 +18,19 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FavouritesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var searchView: SearchView
+    private lateinit var recyclerView: RecyclerView
+
+    private val favBooksListViewModel = FavBooksListViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searchView = view.findViewById(R.id.fragment_favourites_search)
+
+        favBooksListViewModel.fetch {
+            initRecyclerView(view)
         }
     }
 
@@ -35,6 +40,25 @@ class FavouritesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favourites, container, false)
+    }
+
+    private fun initRecyclerView(v: View) {
+
+        recyclerView = v.findViewById<RecyclerView>(R.id.fragment_favourites_recycler_view)
+
+        val adapter = HomeFragment.BookAdapter(
+            ArrayList(FavBooksListModel.getFavBookList().toList()),
+            { book ->
+                val intent = Intent(context, BookDetailsActivity::class.java)
+                intent.putExtra("book", book.toString())
+                startActivity(intent)
+            },
+            { }
+        )
+
+        val layoutManager = GridLayoutManager(context, 2)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
     }
 
     companion object {
@@ -51,8 +75,7 @@ class FavouritesFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             FavouritesFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
